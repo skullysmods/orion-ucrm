6d9bbc6eb0137af49a58efb2deadaec851d3d24d
Skully <117316808+skullysmods@users.noreply.github.com>
2026-04-01 10:24:33 +0200

    Edit 3

--- back/src/test/java/com/openclassroom/devops/orion/microcrm/PersonRepositoryIntegrationTest.java
+++ back/src/test/java/com/openclassroom/devops/orion/microcrm/PersonRepositoryIntegrationTest.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +12,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class PersonRepositoryIntegrationTest {
 
     @Autowired
